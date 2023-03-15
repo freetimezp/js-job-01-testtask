e402a1a09f1cfd89d83f3d124521eb92dbd7d7d0
freetimezp <kraftukrnet@ukr.net>
2023-03-15 22:49:59 +0200

    02. js-job-01-testtask. Home section. Navbar. Menu. Slider.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/* slider */
+var swiper = new Swiper(".homeSwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
