7b5abba353de92635990f25f91763ebb75f57d30
freetimezp <kraftukrnet@ukr.net>
2023-03-16 10:50:17 +0200

    03. js-job-01-testtask. Set menu active.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 /* slider */
 var swiper = new Swiper(".homeSwiper", {
+    spaceBetween: 30,
+    loop: true,
     pagination: {
         el: ".swiper-pagination",
         type: "fraction",
@@ -9,3 +11,18 @@ var swiper = new Swiper(".homeSwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+/* menu active */
+
+const menuItems = document.querySelectorAll(".menu__list-item");
+
+menuItems.forEach(el => el.addEventListener("click", function() {
+    if(el.classList.contains('active')) {
+        el.classList.remove('active');
+    }else{
+        menuItems.forEach(el => el.classList.remove("active"));
+        el.classList.add('active');
+    }
+}))
+
+
